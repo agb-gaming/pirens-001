5d441dc6c76c32dfef327cccb313dd593b542224
agb-gaming <149343872+agb-gaming@users.noreply.github.com>
2025-12-24 07:14:46 +0530

    Create api-rest.py

---
+++ api-rest.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event, context):
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello from AWS REST API!"
+        })
+    }
+
+    return response
